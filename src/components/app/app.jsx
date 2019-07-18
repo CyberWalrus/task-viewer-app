ca0552c8a7ea28939fb2ee5ae7dc30d453ac0f2f
CyberWalrus <stonewalrusfan@gmail.com>
2019-07-19 01:56:31 +0300

    add scss foh hud

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,17 +3,38 @@ import * as React from "react";
 const App = () => {
   return (
     <React.Fragment>
+      <div />
       <header className={`page-header`}>
         <nav className={`filter`}>
           <ul className={`filter__list`}>
-            <li className={`filter__item`}>Все</li>
-            <li className={`filter__item`}>Обычные</li>
-            <li className={`filter__item`}>Важные</li>
-            <li className={`filter__item`}>Очень важные</li>
+            <li className={`filter__item`}>
+              <a href="#" className={`filter__link`}>
+                Все
+              </a>
+            </li>
+            <li className={`filter__item`}>
+              <a href="#" className={`filter__link`}>
+                Обычные
+              </a>
+            </li>
+            <li className={`filter__item`}>
+              <a href="#" className={`filter__link`}>
+                Важные
+              </a>
+            </li>
+            <li className={`filter__item`}>
+              <a href="#" className={`filter__link`}>
+                Очень важные
+              </a>
+            </li>
           </ul>
         </nav>
         <div className={`add-task`}>
-          <button className={`add-task__btn`} />
+          <button className={`add-task__btn`}>
+            <svg viewBox="0 0 448 448">
+              <path d="m408 184h-136c-4.417969 0-8-3.582031-8-8v-136c0-22.089844-17.910156-40-40-40s-40 17.910156-40 40v136c0 4.417969-3.582031 8-8 8h-136c-22.089844 0-40 17.910156-40 40s17.910156 40 40 40h136c4.417969 0 8 3.582031 8 8v136c0 22.089844 17.910156 40 40 40s40-17.910156 40-40v-136c0-4.417969 3.582031-8 8-8h136c22.089844 0 40-17.910156 40-40s-17.910156-40-40-40zm0 0" />
+            </svg>
+          </button>
         </div>
       </header>
       <main className={`page-main`}>
@@ -31,7 +52,7 @@ const App = () => {
                   <p className={`task__id`}>{`1`}</p>
                 </div>
                 <div className={`task__main`}>
-                  <h3 className={`task__title`}></h3>
+                  <h3 className={`task__title`} />
                   <p className={`task__text`}>{item}</p>
                 </div>
                 <div className={`task__controls`}>
