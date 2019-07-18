940628c264e24f21ad808530affaba77563a14bc
Cyber Walrus <stonewalrusfan@gmail.com>
2019-07-18 14:50:47 +0300

    create app

---
+++ src/components/app/app.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+const App = () => {
+  return(
+    <React.Fragment>
+      <head>
+
+      </head>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
+    </React.Fragment>
+  )
+}
